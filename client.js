cba8ab119b5d60efb77613e6ffa421a0d832c509
Pabs <paul.mca.ca@gmail.com>
2023-05-19 02:45:27 +0000

    Created new client.js to house connect function Removed function from play.js and added import function to play.js

---
+++ client.js
@@ -0,0 +1,14 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: "10.0.2.15",// IP address here,
+    port: "50541"// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+  module.exports = { connect }
--- play.js
+++ play.js
@@ -1,17 +1,5 @@
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "10.0.2.15",// IP address here,
-    port: "50541"// PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const { connect } = require('./client')
 
 console.log("Connecting ...");
 connect();
