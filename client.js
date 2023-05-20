d2b2a4c4abd32af679c40daad79aa0385f743b7a
Pabs <paul.mca.ca@gmail.com>
2023-05-20 22:39:07 +0000

    Added timeout event to conn function Debugged

--- client.js
+++ client.js
@@ -2,12 +2,19 @@ const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
     host: "10.0.2.15",// IP address here,
-    port: "50541"// PORT number here,
-  });
+    port: "50541",// PORT number here,
+  }) ;
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on("connect", () => {console.log("Successfully connected to game server")})
+  conn.setTimeout(5000);
+  conn.on('timeout', () => {
+    console.log("you ded cuz you idled");
+    conn.end();
+  }); 
+  
   return conn;
 };
 
