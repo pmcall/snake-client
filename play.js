768386006b049ab4f2d6b538649610bc802544ed
Pabs <paul.mca.ca@gmail.com>
2023-05-21 23:47:40 +0000

    Refactored: Removed setupInput and handleUserInput and redundant net variable

--- play.js
+++ play.js
@@ -1,26 +1,8 @@
-const net = require("net");
-const { connect } = require('./client')
-
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (keyPress) {
-    if (keyPress === '\u0003') {
-      console.log("Thanks for playing!")
-      process.exit();
-    }
-};
-
 setupInput();
+
