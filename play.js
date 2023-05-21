2ca0393a0236a19b89d6e69577ae201c904cddb8
Pabs <paul.mca.ca@gmail.com>
2023-05-21 23:28:44 +0000

    Added setupInput and handleUserInput to handle inputs Currently only input handled is CTRL+C to process.exit

--- play.js
+++ play.js
@@ -1,5 +1,26 @@
 const net = require("net");
 const { connect } = require('./client')
 
+
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (keyPress) {
+    if (keyPress === '\u0003') {
+      console.log("Thanks for playing!")
+      process.exit();
+    }
+};
+
+setupInput();
