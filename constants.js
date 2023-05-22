712b04aa2291b880a649a4a675479e58a6280151
Pabs <paul.mca.ca@gmail.com>
2023-05-22 01:16:31 +0000

    Initialised file to maintain consts Contains: IP&PORT info, messages, movement keys

---
+++ constants.js
@@ -0,0 +1,22 @@
+const IP = "10.0.2.15";
+const PORT = "50541";
+const messages = {
+  "1": "Say: sssssSSSSSsss",
+  "2": "Say: snek",
+  "3": "Say: snake POWER",
+  "4": "Say: nom nom nom",
+  "5": "Say: sssuper sssnake"
+};
+const movement = {
+  "w": "Move: up",
+  "a": "Move: left",
+  "s": "Move: down",
+  "d": "Move: right"
+};
+
+module.exports = {
+  IP,
+  PORT,
+  messages,
+  movement
+};
