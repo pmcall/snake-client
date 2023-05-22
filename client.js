4230e412980cf36f8a737ae09e9b36f7ee7dcba9
Pabs <paul.mca.ca@gmail.com>
2023-05-22 00:28:57 +0000

    Refactor: Updated idle message. Added process.exit after to quit the game. Linted the file

--- client.js
+++ client.js
@@ -1,26 +1,27 @@
 const net = require("net");
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "10.0.2.15",// IP address here,
     port: "50541",// PORT number here,
-  }) ;
+  });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  conn.on("connect", () => { 
-    console.log("Successfully connected to game server")
-  })
-  conn.on("connect", () => { 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
+  conn.on("connect", () => {
     conn.write("Name: PAB");
-  })
+  });
   conn.setTimeout(5000);
-  conn.on('timeout', () => { 
-    console.log("you ded cuz you idled");
+  conn.on('timeout', () => {
+    console.log("You took too long to make a move. Try again.");
     conn.end();
-  }); 
+    process.exit();
+  });
 
   return conn;
 };
 
-  module.exports = { connect }
+module.exports = { connect };
