b8c64f13f6ca2d3951f87e04784a0c1320a1b469
Pabs <paul.mca.ca@gmail.com>
2023-05-22 00:31:49 +0000

    Refactored keypresses to write commands instead of logging them. Added connection variable Connection var to store the active TCP connection object

--- input.js
+++ input.js
@@ -1,29 +1,33 @@
-const setupInput = function () {
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = (conn) => {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
+  connection = conn;
   return stdin;
 };
 
-const handleUserInput = function (keyPress) {
-    if (keyPress === '\u0003') {
-      console.log("Thanks for playing!")
-      process.exit();
-    }
-    if (keyPress === "w") {
-      console.log("Move: up");
-    }
-    if (keyPress === "a") {
-      console.log("Move: left");
-    }
-    if (keyPress === "s") {
-      console.log("Move: down");
-    }
-    if (keyPress === "d") {
-      console.log("Move: right");
-    }
+const handleUserInput = function(keyPress) {
+  if (keyPress === '\u0003') {
+    console.log("Thanks for playing!");
+    process.exit();
+  }
+  if (keyPress === "w") {
+    connection.write("Move: up");
+  }
+  if (keyPress === "a") {
+    connection.write("Move: left");
+  }
+  if (keyPress === "s") {
+    connection.write("Move: down");
+  }
+  if (keyPress === "d") {
+    connection.write("Move: right");
+  }
 };
 
-module.exports = { setupInput }
+module.exports = { setupInput };
